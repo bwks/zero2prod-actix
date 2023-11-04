1a80c1469c496fbdaa3d4ec092c35e46baf22577
Brad Searle <bradleysearle@gmail.com>
2023-11-05 07:45:06 +1000

    readme

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}", &name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("172.31.255.20:7777")?
+    .run()
+    .await
+}
